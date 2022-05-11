b55aba290ca396d17ab623a7e569fa89c8790c5e
“erhaneth” <gumusucb21@gmail.com>
2022-05-10 17:02:28 -0700

    Enter and Delete buttons are functioning.

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ var col = 0;
 
 var gameOver = false;
 //hardcode a solution
-var solution = 'START';
+var solution = 'start';
 
 
 
@@ -37,21 +37,37 @@ var solution = 'START';
     }
 
 
+
 document.getElementById('keyboard-container').addEventListener('click', (event) => {
     console.log(row, col, 'board');
-    board[row][col] = event.target.attributes['data-key'].nodeValue
-   // console.log(board, 'board');
-     //select the tile with id of row-col 
-     let currTile = document.getElementById(`${row}-${col}`)
-     console.log(currTile, 'tile');
+    // console.log(board, 'board');
+    //select the tile with id of row-col 
+    if(event.target.attributes['data-key'].nodeValue === 'enter'){
+        const userGuess =  board[row].join('')
+        if(userGuess === solution){
+            console.log('Correct')
+        }
+    
+        
+    } else if (event.target.attributes['data-key'].nodeValue === 'dlt'){
+        console.log('delete')
+    }else {
+        board[row][col] = event.target.attributes['data-key'].nodeValue
+
+    
+        let currTile = document.getElementById(`${row}-${col}`)
+        console.log(currTile, 'tile');
         // change the inner text to what letter user clicked
         currTile.innerText = board[row][col];
         console.log(currTile.innerText, 'text');
-
+    }  
     
-  
+    
+    
     
     //check board if current letter is matching the letter solution
+    
+
         // if so change the tile to green
         //else if letter is in the current row -- yellow
         //else make it grey
