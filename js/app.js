7b15d93d5de90075340355dd2b36448f74ee58d3
“erhaneth” <gumusucb21@gmail.com>
2022-05-11 19:53:29 -0700

    The bug of button has finally been solved.

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ var gameOver = false;
 
 //hardcode a solution
 var solution = 'start';
-//Creating gameboard
+//Creating  the board
 
    const board = [
                 [' ', ' ', ' ', ' ', ' '],
@@ -36,71 +36,52 @@ var solution = 'start';
 document.getElementById('keyboard-container').addEventListener('click', (event) => {
    
     //select the tile with id of row-col 
-    let currTile = document.getElementById(`${row}-${col}`)
-    console.log(currTile, 'currtileeee')
-    // currTile.style.border = 'orange solid 2px'
+       let currTile = document.getElementById(`${row}-${col}`)
     
-    if(event.target.innerText === 'ENTER'){
+     if(event.target.innerText === 'ENTER'){
         let userGuess =  board[row -1].join('').toLowerCase()
 
         if(userGuess === solution){
         }
-        console.log(board[row - 1])
         checkTileColor(userGuess) 
         
-    // } else if (event.target.innerText === 'DEL'){
-    //     let deleteTile = document.getElementById(`${row}-${col -1}`)
-    //     if(deleteTile){
-    //         //clear the inside of the tile
-    //         deleteTile.innerText = ''
-    //         col -= 1
-    //         //reassign currTile to the tile has been delete
-    //         currTile = deleteTile
     
-    //     }
+        } else {
+        board[row][col] = event.target.innerText
+        // change the inner text to what letter user clicked
+        currTile.innerText = board[row][col];
+        
+        // if col > width we know that we need to make new row 
+        col++;
+        if (col > 4) {
+        row++;
+        col = 0;
+        } 
+      
+     }
     
-    // // console.log(currTile, "any", board, 'boardelete')
-    
-} else {
-    board[row][col] = event.target.innerText
-    
-    
-    console.log(currTile, 'tile');
-    // change the inner text to what letter user clicked
-    currTile.innerText = board[row][col];
-    console.log(currTile.innerText, 'text');
- 
-}
-//check board if current letter is matching the letter solution
-  function checkTileColor(userWord){
+     //check board if current letter is matching the letter solution
+     function checkTileColor(userWord){
+      let checkTile
       for(let r = 0; r < width; r++){
-    // if so change the tile to green
-    if(userWord[r] === solution[r]){
-        currTile.classList.add("green")
-        console.log('solutiongreen', board[r-1])
-        //else if letter is in the current row -- yellow
-    } else if(solution.includes(userWord[r])){
+        checkTile = document.getElementById(`${row-1}-${r}`)
+     // if so change the tile to green
+     if(userWord[r] === solution[r]){
+        checkTile.classList.add("green")
+       
+     //else if letter is in the current row -- yellow
+     } else if(solution.includes(userWord[r])){
 
-        currTile.classList.add("yellow")
+        checkTile.classList.add("yellow")
 
-        //else make it grey
-    } else {
+     //else make it grey
+     } else {
 
-        currTile.classList.add("grey")
+        checkTile.classList.add("grey")
+        }
       }
- }
-} 
-// checkTileColor()
-                  
- // if col > width we know that we need to make new row 
-            
-    col++;
-    if (col > 4) {
-    row++;
-    col = 0;
-    } 
-    console.log(board);
-          
+     } 
+         
 })
 
 
